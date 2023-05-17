78a26574f6d49d8689a91242f7899a51774b87c4
csearafat01 <engr.md.ar@gmail.com>
2023-05-18 03:11:33 +0600

    added new widgets file named icon_text_widgets.dart

--- lib/screens/search_screen.dart
+++ lib/screens/search_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
 import 'package:my_first_flutter_app/utils/app_layout.dart';
+import 'package:my_first_flutter_app/widgets/icon_text_widgets.dart';
 
 import '../utils/app_styles.dart';
 
@@ -9,12 +10,53 @@ class SearchScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final size = AppLayout.getSize(context);
     return  Scaffold(
       backgroundColor: Styles.bgColor,
       body: ListView(
+        padding: EdgeInsets.symmetric(horizontal: AppLayout.getWidth(20),vertical: AppLayout.getHeight(20)),
         children: [
           Gap(AppLayout.getHeight(40)),
-          Text("What Are \nYou Loking For?",style: Styles.headLineStyle1.copyWith(fontSize: 35),)
+          Text("What Are \nYou Loking For?",style: Styles.headLineStyle1.copyWith(fontSize: AppLayout.getWidth(35)),),
+          Gap(AppLayout.getHeight(20)),
+          FittedBox(
+              child: Container(
+                padding: EdgeInsets.all(3.5),
+                child: Row(
+
+                  children: [
+                    /*airline tickets*/
+                    Container(
+                      width: size.width*.44,
+                      padding: EdgeInsets.symmetric(vertical: AppLayout.getHeight(7)),
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.horizontal(left: Radius.circular(AppLayout.getHeight(50)),),
+                          color: Colors.white
+                      ),
+                      child: Center(child: Text("Airline Tickets"),),
+                    ),
+                    /*hotels*/
+                    Container(
+                      width: size.width*.44,
+                      padding: EdgeInsets.symmetric(vertical: AppLayout.getHeight(7)),
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.horizontal(right: Radius.circular(AppLayout.getHeight(50)),),
+                          color: Colors.transparent
+                      ),
+                      child: Center(child: Text("Hotels"),),
+                    )
+                  ],
+                ),
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(AppLayout.getHeight(50),),
+                    color: const Color(0xFFF4F6FD)
+                ),
+              ),
+          ),
+          Gap(AppLayout.getHeight(25)),
+          const AppIconText(icon: Icons.flight_takeoff_rounded, text: 'Departure'),
+          Gap(AppLayout.getHeight(15)),
+          const AppIconText(icon: Icons.flight_land_rounded, text: 'Arrival'),
         ],
       ),
     );
