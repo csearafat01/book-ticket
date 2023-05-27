2aa89324d3e40df812960ffd652effbe5ca25308
csearafat01 <engr.md.ar@gmail.com>
2023-05-28 01:09:32 +0600

    complete profile_screen.dart

--- lib/screens/bottom.dart
+++ lib/screens/bottom.dart
@@ -1,6 +1,7 @@
 import 'package:fluentui_icons/fluentui_icons.dart';
 import 'package:flutter/material.dart';
 import 'package:my_first_flutter_app/screens/home_screen.dart';
+import 'package:my_first_flutter_app/screens/profile_screen.dart';
 import 'package:my_first_flutter_app/screens/search_screen.dart';
 import 'package:my_first_flutter_app/screens/ticket_screen.dart';
 
@@ -17,7 +18,7 @@ class _BottomBarState extends State<BottomBar> {
     const HomeScreen(),
     const SearchScreen(),
     const TicketScreen(),
-    const Text("Profile"),
+    const ProfileScreen(),
 
 
   ];
